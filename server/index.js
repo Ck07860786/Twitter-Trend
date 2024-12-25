c434e950960fc41a61dfee388b2216f40bb0e988
Chotu Kumar <ck4001252@gmail.com>
2024-12-25 22:33:55 +0530

    index file updated

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/trends', trendsRoute);
+app.get('/', (req, res) => {
+    res.send('Welcome to the server!');
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
